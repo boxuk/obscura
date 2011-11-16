c9a9dd4b65cdfde7fff84d2cf71bd7359896128c
Pepe Doval <pepellou@gmail.com>
2011-11-16 01:55:21 +0100

    Cleaned ColorParserTest code

--- tests/php/BoxUK/Obscura/ColorParserTest.php
+++ tests/php/BoxUK/Obscura/ColorParserTest.php
@@ -19,84 +19,67 @@ class ColorParserTest extends \PHPUnit_Framework_TestCase {
     /**
      *
      */
-    public function testParsesArraysToDetermineColor() {
+    public function valid_colors() {
+        return array(
+            "Correct format" => array( array(255, 120, 0, 0), array(255, 120, 0, 0) ),
+            "No transparency (default to opaque)" => array(array(255, 120, 0), array(255,120, 0, 0) )
+        );
+    }
 
-        // Pass in an array in the correct format
-        $color = array( 255, 120, 0, 0 );
-
-        $array = $this->object->getRGBAValuesFromColor($color);
-
-        $this->assertTrue(is_array($array));
-        $this->assertTrue(count($array) === 4);
-        $this->assertSame($color, $array);
-
-        // Pass in an array without a transparency parameter - we should default to opaque
-        $color = array( 255, 120, 0 );
+    /**
+     * @dataProvider valid_colors
+     */
+    public function testParsesArraysToDetermineColor($color, $expected) {
 
         $array = $this->object->getRGBAValuesFromColor($color);
 
         $this->assertTrue(is_array($array));
         $this->assertTrue(count($array) === 4);
-        $this->assertEquals(0, $array[3]);
-
-        // Pass in an array with too few arguments - exception should be thrown
-        try {
-            $color = array(10, 20);
-            $this->object->getRGBAValuesFromColor($color);
-            $this->fail();
-        }
-        catch(\InvalidArgumentException $expected) {
-            // pass
-        }
-
-        // Pass in an array with too many arguments - exception should be thrown
-        try {
-            $color = array(10, 20, 30, 40, 50);
-            $this->object->getRGBAValuesFromColor($color);
-            $this->fail();
-        }
-        catch(\InvalidArgumentException $expected) {
-            //pass
-        }
+        $this->assertSame($expected, $array);
 
     }
 
     /**
      *
      */
-    public function testParsesHexadecimalStringsAndDeterminesColor() {
+    public function invalid_colors() {
+        return array(
+            "Too few arguments"  => array(array(10, 20)),
+            "Too many arguments" => array(array(10, 20, 30, 40, 50)),
+            "Nonsense string"    => array('somenonsensestring')
+        );
+    }
 
-        // Test parsing a hexadecimal color
-        $color = 'FF9900';
+    /**
+     * @dataProvider invalid_colors
+     * @expectedException InvalidArgumentException
+     */
+    public function testFailsParsingInvalidColors($invalid_color) {
+
+        $this->object->getRGBAValuesFromColor($invalid_color);
+
+    }
+
+    /**
+     *
+     */
+    public function hexadecimal_colors() {
+        return array(
+            "Hexadecimal color"  => array( 'FF9900',  array(255, 153, 0, 0) ),
+            "Web format"         => array( '#CCCCCC', array(204, 204, 204, 0) )
+        );
+    }
+
+    /**
+     * @dataProvider hexadecimal_colors
+     */
+    public function testParsesHexadecimalStringsAndDeterminesColor($color, $expected) {
 
         $array = $this->object->getRGBAValuesFromColor($color);
 
         $this->assertTrue(is_array($array));
         $this->assertTrue(count($array) === 4);
-        $this->assertEquals(255, $array[0]);
-        $this->assertEquals(153, $array[1]);
-        $this->assertEquals(0, $array[2]);
-        $this->assertEquals(0, $array[3]); // No way to specify transparency, should default to opaque
-
-        // Some people may use web-format
-        $color = '#CCCCCC';
-
-        $array = $this->object->getRGBAValuesFromColor($color);
-
-        $this->assertTrue(is_array($array));
-        $this->assertTrue(count($array) === 4);
-        $this->assertEquals(204, $array[0]);
-        $this->assertEquals(204, $array[1]);
-        $this->assertEquals(204, $array[2]);
-        $this->assertEquals(0, $array[3]);
-
-        // Try some nonsense
-        try {
-            $color = 'somenonsensestring';
-            $this->object->getRGBAValuesFromColor($color);
-            $this->fail();
-        }
-        catch(\InvalidArgumentException $expected) {}
+	$this->assertSame($expected, $array);
 
     }
 }
